dfdc641b07d801ec2d2be73bbf89757cab1f49ba
navitapatil <64483499+navitapatil@users.noreply.github.com>
2023-09-04 22:45:56 +0530

    Update ball.py
    done

--- ball.py
+++ ball.py
@@ -1,4 +1,3 @@
-from turtle import Turtle
 class Ball(Turtle):
 
     def __init__(self):
@@ -8,12 +7,20 @@ class Ball(Turtle):
         self.penup()
         self.x_move = 10
         self.y_move = 10
+        self.move_speed = 0.1
 
     def move(self):
         new_x = self.xcor() + self.x_move
-        new_y = self.xcor() + self.y_move
+        new_y = self.ycor() + self.y_move
         self.goto(new_x, new_y)
 
-    def bounce(self):
+    def bounce_y(self):
         self.y_move *= -1
-        print(self.y_move)
+
+    def bounce_x(self):
+        self.x_move *= -1
+        self.move_speed *= 0.9
+
+    def reset_position(self):
+        self.goto(0,0)
+        self.bounce_x()   #reverses the ditrection towards left
