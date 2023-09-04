bbcca25a7d207d42f4a7f1825ee2d4b5193253b2
navitapatil <64483499+navitapatil@users.noreply.github.com>
2023-09-04 22:45:14 +0530

    Update main.py
    done main

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import time
 from turtle import Screen, Turtle
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
+import time
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -13,6 +15,7 @@ screen.tracer(0)   #doesnt show animation
 r_paddle = Paddle((350,0))
 l_paddle = Paddle((-350,0))
 ball = Ball()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -22,16 +25,26 @@ screen.onkey(l_paddle.go_down,"s")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()  #updates screen everytime game is on
     ball.move()
 
     #collision detection with wall
     if ball.ycor() > 280 or ball.ycor() < -280:
-        ball.bounce()
+        ball.bounce_y()
 
     #dect collision with r_paddle
-    if ball.distance(r_paddle) < 50 and ball.xcor() > 340:
-        print("made contact")
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    #Detect when right paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()    #method in ball
+        scoreboard.l_point()
+
+    #Detect when left paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
 
 screen.exitonclick()
